37f934cda903938cea1449c3ef6ffeda071bf587
Jan Nold <jan.nold@udo.edu>
2016-07-16 18:09:35 +0200

    sensor control added

---
+++ sensorcontrol.js
@@ -0,0 +1,7 @@
+'use strict';
+let request = require('request') // library to make http requests
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0" // accept the selfsig cert
+
+
+//let baseurl = "https://192.168.193.1:8443/rest/devices/" <- TODO
