17924904af9fd21ee3edee88e4dc9da42f4316ad
whoopwh00p <stefanczubek@web.de>
2016-07-17 00:05:34 +0200

    FaceRecognition

---
+++ public/EsWerdeLicht/web/faceDetection.js
@@ -0,0 +1,79 @@
+function enableLightsByLocation(coords)
+{
+    var vHeight = parseInt($("#localVideo").height());
+    var vWidth = parseInt($("#localVideo").width());
+    var offsetTop = parseInt($("#localVideo").offset().top);
+    var offsetLeft = parseInt($("#localVideo").offset().left);
+    var x = (coords.x/2);
+    var y = (coords.y/2);
+    
+    if(x <= (vWidth+offsetLeft) / 2 && y <= (vWidth+offsetLeft))
+    {
+       $("#quarterLabel").text("FIRST");
+         var xhr = new XMLHttpRequest();
+         xhr.open("GET", "http://localhost:8081/setlight/off/"+0, false);
+         xhr.setRequestHeader("Content-Type", "application/json");
+         xhr.send(null); 
+                  xhr.open("GET", "http://localhost:8081/setlight/on/"+1, false);
+         xhr.setRequestHeader("Content-Type", "application/json");
+         xhr.send(null); 
+         xhr.open("GET", "http://localhost:8081/setlight/on/"+2, false);
+         xhr.setRequestHeader("Content-Type", "application/json");
+         xhr.send(null); 
+         xhr.open("GET", "http://localhost:8081/setlight/on/"+3, false);
+         xhr.setRequestHeader("Content-Type", "application/json");
+         xhr.send(null); 
+    }
+    if(x > (vWidth+offsetLeft) / 2 && y <= (vHeight+offsetTop) / 2)
+    {
+        $("#quarterLabel").text("SECOND");
+         var xhr = new XMLHttpRequest();
+         xhr.open("GET", "http://localhost:8081/setlight/off/"+1, false);
+         xhr.setRequestHeader("Content-Type", "application/json");
+         xhr.send(null); 
+         xhr.open("GET", "http://localhost:8081/setlight/on/"+0, false);
+         xhr.setRequestHeader("Content-Type", "application/json");
+         xhr.send(null); 
+         xhr.open("GET", "http://localhost:8081/setlight/on/"+2, false);
+         xhr.setRequestHeader("Content-Type", "application/json");
+         xhr.send(null); 
+         xhr.open("GET", "http://localhost:8081/setlight/on/"+3, false);
+         xhr.setRequestHeader("Content-Type", "application/json");
+         xhr.send(null); 
+    }
+    if(x <= (vWidth+offsetLeft) / 2 && y > (vHeight+offsetTop) / 2) {
+        $("#quarterLabel").text("THIRD");
+         var xhr = new XMLHttpRequest();
+         xhr.open("GET", "http://localhost:8081/setlight/off/"+2, false);
+         xhr.setRequestHeader("Content-Type", "application/json");
+         xhr.send(null); 
+                  xhr.open("GET", "http://localhost:8081/setlight/on/"+0, false);
+         xhr.setRequestHeader("Content-Type", "application/json");
+         xhr.send(null); 
+         xhr.open("GET", "http://localhost:8081/setlight/on/"+1, false);
+         xhr.setRequestHeader("Content-Type", "application/json");
+         xhr.send(null); 
+         xhr.open("GET", "http://localhost:8081/setlight/on/"+3, false);
+         xhr.setRequestHeader("Content-Type", "application/json");
+         xhr.send(null); 
+    }
+    
+    if(x > (vWidth+offsetLeft) / 2 && y > (vHeight+offsetTop) / 2)
+    {
+        $("#quarterLabel").text("FOURTH");
+         var xhr = new XMLHttpRequest();
+            xhr.open("GET", "http://localhost:8081/setlight/off/"+3, false);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(null);      
+            xhr.open("GET", "http://localhost:8081/setlight/on/"+0, false);
+         xhr.setRequestHeader("Content-Type", "application/json");
+         xhr.send(null); 
+         xhr.open("GET", "http://localhost:8081/setlight/on/"+2, false);
+         xhr.setRequestHeader("Content-Type", "application/json");
+         xhr.send(null); 
+         xhr.open("GET", "http://localhost:8081/setlight/on/"+1, false);
+         xhr.setRequestHeader("Content-Type", "application/json");
+         xhr.send(null); 
+            
+    }
+}
