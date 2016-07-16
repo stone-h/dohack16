66e9edc95105ecfc0dea1b66b1510d974ae5cead
whoopwh00p <stefanczubek@web.de>
2016-07-16 16:31:06 +0200

    Revert "getilgt : status information"
    This reverts commit 050387dc3821171432fb88de2cd2b5e3f30a95a8.

--- server.js
+++ server.js
@@ -14,38 +14,15 @@ server.get('/', (req, res) => {
 })
 
 // set light on with id
-server.get('/getlight/:id', (req, res) => {
-  let lightid = req.params.id
-  if(lightid < 4 && lightid >= 0){
-    lightcontroller.getLight(lightid, (value) => {
-      console.log("va: " + value)
-      res.json({on: value})
-    })
-  }else{
-    res.send(404)
-  }
-})
-
-// set light on with id
 server.get('/setlight/on/:id', (req, res) => {
   let lightid = req.params.id
-  if(lightid < 4 && lightid >= 0){
-    lightcontroller.setLight(lightid, true)
-    res.send(200)
-  }else{
-    res.send(404)
-  }
+  lightcontroller.setLight(lightid, true)
 })
 
 // set light off with id
 server.get('/setlight/off/:id', (req, res) => {
   let lightid = req.params.id
-  if(lightid < 4 && lightid >= 0){
-    lightcontroller.setLight(lightid, false)
-    res.send(200)
-  }else{
-    res.send(404)
-  }
+  lightcontroller.setLight(lightid, false)
 })
 
 server.listen(8081)
